6a6d900718c6977000ca5b94a5bbe5569162e70f
shinsukematsuda <smatsuda@x-hack.jp>
2021-08-22 13:40:01 +0900

    hosts追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -7,6 +7,8 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+
+  config.hosts << 'dev.at-coupon.com'
 
   # Do not eager load code on boot.
   config.eager_load = false
